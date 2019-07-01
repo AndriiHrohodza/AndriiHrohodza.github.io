f99e890bdb7ec5ac4e133e77340385455aa208b0
AndriiHrohodza <ahrohodza1@gmail.com>
2019-07-01 21:58:30 +0300

    work with slider

--- DSS/js/jsDSS.js
+++ DSS/js/jsDSS.js
@@ -2,15 +2,15 @@ const flightPath = {
 	curviness: 1.25,
 	autoRotate: true,
 	values: [
-	 {x: 100, y: 0},
-	 {x: window.innerWidth, y: 0},
+	 {x:950, y: 0},
+	 {x:1290, y: 0}
 	]
 };
 
 const tween = new TimelineLite();
 
 tween.add(
-	TweenLite.to('.animation', 1, {
+	TweenLite.to('.features-content', 1, {
 		bezier: flightPath,
 		ease: Power1.easeInOut
 	})
@@ -19,12 +19,12 @@ tween.add(
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-	triggerElement: '.about-company',
+	triggerElement: '.features',
 	duration: 1000,
-	/*triggerHook: 0*/
+	triggerHook: 0.2
 
 })
 .setTween(tween)
 .addIndicators()
-.setPin('.about-company')
+.setPin('.features')
 .addTo(controller);
