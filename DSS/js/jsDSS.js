0aeabc3b2a564a1fe679633006885a3eb3df1e5f
AndriiHrohodza <ahrohodza1@gmail.com>
2019-07-03 09:38:02 +0300

    reload files

---
+++ DSS/js/jsDSS.js
@@ -0,0 +1,54 @@
+/*const flightPath = {
+	curviness: 1.25,
+	autoRotate: true,
+	values: [
+	 {x:window.innerWidth, y: 0},
+	 {x:0, y: 0},
+	 {x:window.innerWidth, y: 0}
+	]
+};
+
+const tween = new TimelineLite();
+
+tween.add(
+	TweenLite.to('.features-content', 1, {
+		bezier: flightPath,
+		ease: Power1.easeInOut,
+	})
+	);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+	triggerElement: '.features',
+	duration: 1000,
+	triggerHook: 0.2
+
+})
+.setTween(tween)
+.addIndicators()
+.setPin('.features')
+.addTo(controller);*/
+
+
+/*var controller = new ScrollMagic.Controller();
+
+// build tween
+	var tween = TweenMax.staggerFromTo(".features-content", 2, {left: 900}, {left: 0, ease: Back.easeOut}, 0.15);
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 300})
+					.setTween(tween)
+					.addIndicators({name: "staggering"}) // add indicators (requires plugin)
+					.addTo(controller);*/
+
+/*Scroll*/
+/*function slowScroll(id) { 
+               var offset = 0;
+               $('html, body').animate({ 
+                    scrollTop: $(id).offset().top - offset 
+               }, 1000);
+               return false; 
+           }*/
+
+           
