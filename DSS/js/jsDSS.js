e736479524988ca568e7c81a61a1467f7691577e
AndriiHrohodza <ahrohodza1@gmail.com>
2019-07-04 15:03:50 +0300

    change scroll

--- DSS/js/jsDSS.js
+++ DSS/js/jsDSS.js
@@ -30,20 +30,22 @@ const scene = new ScrollMagic.Scene({
 .setPin('.features')
 .addTo(controller);*/
 
+/*Animation Scroll*/
 $(document).ready(function(){
 var controller = new ScrollMagic.Controller();
 // pin 
 var pinFeatureScene = new ScrollMagic.Scene({
 	triggerElement: '#features-pin',
-	triggerHook: 0.2,
+	triggerHook: 0.25,
 	duration: '150%'
 })
 .setPin('#features-pin')
 .setTween(TweenMax.staggerFromTo(".features-content", 2, {left: '50vw'}, {left: '-80vw', easeInOut: Back.easeOut}, 0.15))
 /*.addIndicators({name: "pin"})*/
 .addTo(controller);
-
 });
+
+/**/
 
 
 	
