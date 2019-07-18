061b2f489e50c16df51b25e3ce187cc84b4e3716
AndriiHrohodza <ahrohodza1@gmail.com>
2019-07-18 19:17:44 +0300

    created tabs

--- Makbush/js/mainJS.js
+++ Makbush/js/mainJS.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
 /*Back-wrapper*/
 	$('li .opacity_dark').eq(0).addClass('active')
+	$('li .js-primary-tabs').eq(0).addClass('show')
 	/**/
 	$('ul .slider-pagination__item').click(function(){
 		var attr = $(this).find('img').attr('src')
 		var index = $(this).index()
 		$('.wrapper-back').css('background-image','url'+'('+ attr +')')
 		$('li .opacity_dark').removeClass('active').eq(index).addClass('active')
+		$('li .js-primary-tabs').removeClass('show').eq(index).addClass('show')
 	})
 
 /*Front-wrapper*/
 	$('li .opacity_dark').eq(0).addClass('active')
+	$('li.js-primary-tabs').eq(0).addClass('show')
 	/**/
 	$('ul .slider-pagination__item').click(function(){
 		var attr = $(this).find('img').attr('src')
 		var index = $(this).index()
 		$('.wrapper-back .wrapper-front').css('background-image','url'+'('+ attr +')')
 		$('li .opacity_dark').removeClass('active').eq(index).addClass('active')
+		$('li.js-primary-tabs').removeClass('show').eq(index).addClass('show')
 	})
 
+	/*$(".slider-pagination").on("click", ".slider-pagination__item", function(e) {
+            	e.preventDefault();
+            	$(".slider-pagination__item").removeClass("active");
+            	$(".js-primary-tabs").removeClass("show");
+            	$(this).addClass("active");
+            	$($(this).attr("href")).addClass("show");
+            });*/
 
 
 })
+
+
 
 /*$(function(){
 	$('ul.slider-pagination li:first').addClass('active');
