987664582c94f81c3bd76d5311d3e7fd6dc08510
AndriiHrohodza <ahrohodza1@gmail.com>
2019-08-21 12:50:12 +0300

    commercial project

---
+++ DSS/js/video.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function() {
+	$(function(){
+		function callback () {
+			document.querySelector('video').src = document.querySelector('video').src
+			document.querySelector('video').play();
+		}
+		window.addEventListener("load", callback, false);
+
+	});
+});*/
